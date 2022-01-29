57992b02faea2ac310c9103e8acc4d25875d106a
HEI <ysh@cneccc.local>
2022-01-29 10:50:54 +0800

    learn setting from next three starter #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,33 @@ import type { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 import { Preload } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-const modelUrl = '/ninja.gltf';
+import partition from '@/helpers/partition'
+
+const Ninja = dynamic(() => import('@/components/Ninja'), {
+    ssr: false,
+});
 
 
+
+// canvas components goes here
+const R3F = () => {
+    return (
+      <>
+        <Ninja />
+      </>
+    )
+  }
 
 const Home: NextPage = () => {
 
-    //const nodes = useGLTF(modelUrl);
-    
-
     return (
         <>
-            123
             <Canvas
                 mode='concurrent'
+                style={{ background: 'blue', width: '300px', height: '300px', clipPath: 'circle(50% at 50% 50%)' }}
             >
                 <Preload all />
+                <R3F  />
             </Canvas>
         </>
     )
